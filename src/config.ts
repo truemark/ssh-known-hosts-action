dabad052bc32e3d61ac4caa741f916d27ea405a9
Erik Jensen <365211+erikrj@users.noreply.github.com>
2023-04-07 17:31:02 -0600

    Adding initial sources

---
+++ src/config.ts
@@ -0,0 +1,54 @@
+import {getInput} from '@actions/core'
+
+export function required(value: string | undefined | null): string {
+  if (!value) throw new Error('Required input is missing')
+  return value
+}
+
+export function optionalNumber(value: string): number | undefined {
+  if (!value) return undefined
+  return parseInt(value, 10)
+}
+
+export function optionalBoolean(value: string): boolean | undefined {
+  if (!value) return undefined
+  return value === 'true'
+}
+
+export function optionalJson<T>(value: string): T | undefined {
+  if (!value) return undefined
+  return JSON.parse(value) as T
+}
+
+export function optionalValues(value: string, allowedValues: string[]): string | undefined {
+  if (!value) return undefined
+  if (!allowedValues.includes(value)) throw new Error(`Invalid value for input: ${value}`)
+  return value
+}
+
+export function optionalArray(value: string, delimiter?: string): string[] | undefined {
+  if (!value) return undefined
+  return value.split(delimiter ?? '\n')
+}
+
+export function undefinedIfEmpty(value: string): string | undefined {
+  if (!value || value === '') return undefined
+  return value
+}
+
+export function requiredValues(value: string, allowedValues: string[]): string {
+  if (!allowedValues.includes(value)) throw new Error(`Invalid value for input: ${value}`)
+  return value
+}
+
+export interface Config {
+  readonly entries: string[]
+  readonly scanHosts: string[]
+}
+
+export function loadConfig(): Config {
+  return {
+    entries: optionalArray(getInput('entries')) ?? [],
+    scanHosts: optionalArray(getInput('scan-hosts')) ?? []
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {loadConfig} from './config'
+import * as path from 'path'
+import * as fs from 'fs'
+import {execaSync} from 'execa'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const config = loadConfig()
+    const entries = []
+    for (const scanHost of config.scanHosts) {
+      const parts = scanHost.split(':')
+      const host = parts[0]
+      const port = parts.length === 1 ? '22' : parts[1]
+      const result = execaSync('ssh-keyscan', ['-p', port, host])
+      entries.push(...result.stdout.split('\n'))
+    }
+    entries.push(...config.entries)
+    const sshDir = path.join(process.env['HOME'] ?? '', '.ssh')
+    if (!fs.existsSync(sshDir)) {
+      fs.mkdirSync(sshDir, {recursive: true})
+      fs.chmodSync(sshDir, '700')
+    }
+    const knownHostsFile = path.join(sshDir, 'known_hosts')
+    const stream = fs.createWriteStream(knownHostsFile, {flags: 'a', encoding: 'utf8'})
+    stream.write(entries.join('\n'))
+    stream.end('\n')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
